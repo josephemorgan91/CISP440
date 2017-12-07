0d290b414b435b57637fd1e7609b440a82385103
Joe <josephemorgan91@gmail.com>
2017-12-07 04:34:52 -0800

    Finished hw 13.42

--- sm/sm.cpp
+++ sm/sm.cpp
@@ -1,70 +1,34 @@
-/*
-	A state machine to find all occurances of the string "abab"
-
-	input file "abab.txt":
-	aaaaaababbaaaababababaabaabbbbaabbabbabababbbbbbbabbababaaabaabbab
-
-	output:
-	aaaaaabab8baaaabab16ab18ab20aabaabbbbaabbabbabab40ab42bbbbbbabbabab55aaabaabbab
-	
-	Dan Ross
-	April 2013
-*/
 #include <iostream>
 #include <fstream>
 #include <cstring>
-using namespace std;
 
-#define STATES 20    // room for cream
-// State Table for:  "abab"
-//            [state][ch] 
-char nextState[STATES][2] = {
-//     a  b   //input
-       1, 0,  //state 0
-	   1, 2,  //state 1
-	   3, 0,  //state 2
-	   1, 4,  //state 3
-	   3, 0,  //state 4
-};
+#define STATES 40
 
-char state = 0;
+char nextState[STATES][2]; // = {{1, 0},{1, 2},{3, 0},{1, 4},{3, 0},};
+unsigned char state = 0;
 int count = 0;
-
-
-//////////////////////////////////////////////////
-//  BEGIN SECTION 42: STUFF YOU MAY NEED LATER
-//////////////////////////////////////////////////
-
-// hard code input string
-char instr[80] = "abab";  
-
+char instr[80];
 
 void print_StateTable()
-{
-	cout << "s\ta\tb\n\n";
-	
+{ 
+	printf ("    s    a    b\n\n");
+
 	int l = strlen(instr) + 1;
 	for(int s = 0; s < l; s++) {
-		cout << s << "\t";
+		printf ("%5i", s);
 		for(int ch = 0; ch < 2; ch++)
-			cout << (int) nextState[s][ch] << "\t";
-		cout << endl;
+			printf ("%5i", (int)nextState[s][ch]);
+		printf ("\n");
 	}
 }
 
-
-/*
-Compares first n characters of str1,
-with last n characters of str2.
-
-Returns 0 (false) if they match
-
-Copyright (c) 1981 Dan Ross
-*/
 int strncmp_olap(char * str1, char * str2, int n)
 {
+	// Compares first n characters of str1,
+	// with last n characters of str2.
+	// Returns 0 (false) if they match
 	int i, j;
-	int l1 = strlen(str1);
+	//int l1 = strlen(str1);
 	int l2 = strlen(str2);
 
 	for(i = 0, j = l2 - n; i < n ; i++, j++)
@@ -74,64 +38,82 @@ int strncmp_olap(char * str1, char * str2, int n)
 	return false;
 }
 
-
-/*
-Generate a state table for a sequence detector
-*/
-void generate_StateTable(void)
+void strclean (char str[], int size)
 {
-	// you write this
-
-	/* you may need these things...
-	char got[80];
-	got[0] = 0;         // the null terminator starring the Governator!
-	strncpy(...);       // handy, but it doesn't do this: got[zeroSpot] = 0;
-	strcat(...);	    // for sticking on a's and b's
-	strlen(...);		// is useful too
-	strncmp_olap(..)	// an amazing function!  try it!
-	*/
+	for (int i = 0; i < size; ++i)
+		str[i] = '\0';
 }
 
+void generate_StateTable(void)
+{
+	char got[80];
+	strclean (got, 80);
+	int max_olap = 0;
+	printf("Generating state table for %s...\n", instr);
+	for (char c = 'a'; c <= 'b'; ++c) {
+		for (unsigned s = 0; s < (strlen (instr) + 1); ++s) {
+			strcpy (got, instr);
+			got[s + 1] = '\0';
+			got[s] = c;
+			max_olap = 0;
+			for (size_t x = 1; x <= strlen(instr) && x <= strlen(got); ++x)
+				if (!strncmp_olap (instr, got, x))
+					max_olap = x;
+			printf ("State: %i  -  Input: %c  -  Next State: %i\n", s, c, max_olap);
+			nextState[s][c - 97] = max_olap;
+			strclean(got, 80);
+		}
+	}
+}
 
-//////////////////////////////////////////////
-//  END SECTION 42
-//////////////////////////////////////////////
-
-
-
-// A State Machine
 void process(char ch)
 {
-	// print current char
-	cout << ch;
-	
 	// move to next state
 	state = nextState[state][ch - 'a'];
 
 	// output count if we are in the accept state
-	if(state == 4) cout << count + 2;  // +2 for notepad
+	if(state == strlen(instr)) 
+		printf("%i ", (count + 2));  // +2 for notepad
 
 	count++;  //another ch bites the dust!
 }
 
-int main()
+void clean_and_run (char str[])
 {
-	generate_StateTable();  // you write
-	print_StateTable();
-
 	char ch;
-	
-	// open a file
-	ifstream in("abab.txt");
+	for (int i = 0; i < STATES; ++i)
+		for (int j = 0; j < 2; ++j)
+			nextState[i][j] = 0;
+	count = 0;
+	strcpy (instr, str);
+	state = 0;
+	generate_StateTable();
+	print_StateTable();
+	std::ifstream in("./monkeydata.txt");
 	if(!in){
-		cout << "Error opening file";
+		printf ("Error opening file\n");
 	}
-
-	// read characters from file and process them
+	printf ("The search string was found in the following locations: ");
 	while(in){
 		in.get(ch);
 		if(in)
 			process(ch);
 	}
+	printf ("\n");
+	in.close();
+}
 
+int main()
+{
+	char inputs[5][80] =
+	{
+		"abbbbbbaaaaaba",
+		"abbbbaabbbbaaa",
+		"aaaaaaabaaaaaaa",
+		"aabbbababaaababbaaaa",
+		"aaaabababaaaaabbaaaa"
+	};
+
+	for (int i = 0; i < 5; ++i)
+		clean_and_run (inputs[i]);
 }
